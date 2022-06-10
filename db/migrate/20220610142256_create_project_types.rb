ecfa829f3dc792c113c6912e13e9adbc3e1e085c
abdulkhaliqdev <abdulkhaliqdev@gmail.com>
2022-06-10 19:24:15 +0500

    Add Project Type model

---
+++ db/migrate/20220610142256_create_project_types.rb
@@ -0,0 +1,10 @@
+class CreateProjectTypes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :project_types do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
