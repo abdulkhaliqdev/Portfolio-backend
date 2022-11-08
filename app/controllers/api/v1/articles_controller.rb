8f6be98a43d2b1d69e1f20a3009aa803cda96ffd
abdulkhaliqdev <abdulkhaliqdev@gmail.com>
2022-11-09 02:35:39 +0500

    Add User index functionality

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,21 +1,19 @@
 class Api::V1::ArticlesController < Api::BaseController
-  before_action :find_article, only: [:show]
-
   def index
-    @articles = Article.all
-    return render json: {errors: 'There is no Project Yet'}, status: '401' if @articles.empty?
-    render json: @articles
+    render json: articles
   end
 
   def show
-    render json: @article
+    render json: article
   end
 
   private
 
-  def find_article
-    @article = Article.find_by!(id: params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render json: { errors: "There is no Article yet."}, status: 404
+  def articles
+    @articles ||= Article.all
+  end
+
+  def article
+    @article ||= Article.find_by!(id: params[:id])
   end
 end
--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,21 +1,19 @@
 class Api::V1::ProjectsController < Api::BaseController
-  before_action :find_project, only: [:show]
-
   def index
-    @projects = Project.all
-    return render json: {errors: 'There is no Project Yet'}, status: '401' if @projects.empty?
-    render json: @projects
+    render json: projects
   end
 
   def show
-    render json: @project
+    render json: project
   end
 
   private
 
-  def find_project
-    @project = Project.find_by!(id: params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render json: { errors: "There is no Project yet."}, status: 404
+  def projects
+    @project ||= Project.all
+  end
+
+  def project
+    @project ||= Project.find_by!(id: params[:id])
   end
 end
